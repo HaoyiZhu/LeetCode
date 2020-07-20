d2e8dc6371c9b4ac74566cb78bb75ad37b9d44a4
haoyi_zhu <20001108zhuhaoyi@163.com>
2020-07-20 23:58:48 +0800

    add an exercise

---
+++ Linked List/simple/21.cpp
@@ -0,0 +1,63 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+//Sol1
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* tail = head;
+        while(l1 && l2)
+        {
+            if(l1->val < l2->val)
+            {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        if(l1) tail->next = l1;
+        if(l2) tail->next = l2;
+        return head->next;
+    }
+};
+
+//Sol2
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* preHead = new ListNode(-1);
+
+        ListNode* prev = preHead;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+
+        prev->next = l1 == nullptr ? l2 : l1;	// this step can largely save time (20ms -> 4ms)!!!
+
+        return preHead->next;
+    }
+};
